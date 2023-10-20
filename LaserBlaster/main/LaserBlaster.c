9a231ca47400fa249c9b0a791b2bd769ee3c049b
delta-12 <btesq235@gmail.com>
2023-10-19 20:55:08 -0400

    11-buttons-and-other-digital-inputs initialize gpio for buttons

--- LaserBlaster/main/LaserBlaster.c
+++ LaserBlaster/main/LaserBlaster.c
@@ -1,59 +1,16 @@
-#include "driver/gpio.h"
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
-#include "freertos/queue.h"
-#include <inttypes.h>
+#include "Gpio.h"
 #include <stdio.h>
 
-#define BUTTON_0_PIN GPIO_NUM_18
-#define BUTTON_1_PIN GPIO_NUM_19
-#define BUTTON_2_PIN GPIO_NUM_21
-#define BUTTON_PIN_SEL ((1UL << BUTTON_0_PIN) | (1UL << BUTTON_1_PIN) | (1UL << BUTTON_2_PIN))
-
-#define ESP_INTR_FLAG_DEFAULT 0
-
-static QueueHandle_t gpio_evt_queue = NULL;
-
-static void IRAM_ATTR gpio_isr_handler(void *arg)
-{
-    uint32_t gpio_num = (uint32_t)arg;
-    xQueueSendFromISR(gpio_evt_queue, &gpio_num, NULL);
-}
-
-static void gpio_task_example(void *arg)
-{
-    uint32_t io_num;
-    for (;;)
-    {
-        if (xQueueReceive(gpio_evt_queue, &io_num, portMAX_DELAY))
-        {
-            printf("GPIO[%" PRIu32 "] intr, val: %d\n", io_num, gpio_get_level(io_num));
-        }
-    }
-}
+static void ButtonEventHandler(const Gpio_GpioNum_t gpioNum);
 
 void app_main(void)
 {
-    gpio_config_t buttons = {
-        .pin_bit_mask = BUTTON_PIN_SEL,
-        .mode = GPIO_MODE_INPUT,
-        .pull_up_en = GPIO_PULLUP_DISABLE,
-        .pull_down_en = GPIO_PULLDOWN_DISABLE,
-        .intr_type = GPIO_INTR_NEGEDGE,
-    };
-    gpio_config(&buttons);
+    Gpio_Init();
 
-    // create a queue to handle gpio event from isr
-    gpio_evt_queue = xQueueCreate(10, sizeof(uint32_t));
-    // start gpio task
-    xTaskCreate(gpio_task_example, "gpio_task_example", 2048, NULL, 10, NULL);
+    Gpio_RegisterEventHandler(GPIO_TYPE_BUTTON, ButtonEventHandler);
+}
 
-    // install gpio isr service
-    gpio_install_isr_service(ESP_INTR_FLAG_DEFAULT);
-    // hook isr handler for specific gpio pin
-    gpio_isr_handler_add(BUTTON_0_PIN, gpio_isr_handler, (void *)BUTTON_0_PIN);
-    // hook isr handler for specific gpio pin
-    gpio_isr_handler_add(BUTTON_1_PIN, gpio_isr_handler, (void *)BUTTON_1_PIN);
-    // hook isr handler for specific gpio pin
-    gpio_isr_handler_add(BUTTON_2_PIN, gpio_isr_handler, (void *)BUTTON_2_PIN);
+static void ButtonEventHandler(const Gpio_GpioNum_t gpioNum)
+{
+    printf("Button %ld pressed.\n", gpioNum);
 }
---
+++ LaserBlaster/main/include/Gpio.h
@@ -0,0 +1,35 @@
+/**
+ * @file Gpio.h
+ *
+ * @brief Manage GPIO peripherals.
+ *
+ ******************************************************************************/
+
+/* Includes
+ ******************************************************************************/
+#include <stdint.h>
+
+/* Defines
+ ******************************************************************************/
+
+#define GPIO_BUTTON_0 GPIO_NUM_18
+#define GPIO_BUTTON_1 GPIO_NUM_19
+#define GPIO_BUTTON_2 GPIO_NUM_21
+#define GPIO_BUTTON_PIN_SEL ((1UL << GPIO_BUTTON_0) | (1UL << GPIO_BUTTON_1) | (1UL << GPIO_BUTTON_2))
+
+/* Typedefs
+ ******************************************************************************/
+
+typedef uint32_t Gpio_GpioNum_t;                                   /* GPIO number */
+typedef void (*Gpio_EventHandler_t)(const Gpio_GpioNum_t gpioNum); /* GPIO event handler */
+
+typedef enum
+{
+    GPIO_TYPE_BUTTON, /* GPIO input for buttons */
+} Gpio_Type_t;        /* Type of physical device or sensor connected to GPIO */
+
+/* Function Prototypes
+ ******************************************************************************/
+
+void Gpio_Init(void);
+void Gpio_RegisterEventHandler(const Gpio_Type_t gpioType, Gpio_EventHandler_t eventHandler);
