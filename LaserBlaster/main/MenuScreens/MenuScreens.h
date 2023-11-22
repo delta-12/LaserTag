977d11ebfa1780a33b393bc566d70132d5267b12
delta-12 <btesq235@gmail.com>
2023-11-22 15:45:21 -0500

    82-display-menus directory for menu screens

---
+++ LaserBlaster/main/MenuScreens/MenuScreens.h
@@ -0,0 +1,11 @@
+/**
+ * @file MenuScreens.h
+ *
+ * @brief Screens for menu displayed on OLED.
+ *
+ ******************************************************************************/
+
+#ifndef MENU_SCREENS_H
+#define MENU_SCREENS_H
+
+#endif
