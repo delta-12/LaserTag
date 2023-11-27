c5c9d6cf04d017318e7cacd64545c7c5dd2841e8
Kushal Parekh <98786169+KushalP17@users.noreply.github.com>
2023-11-26 21:10:29 -0500

    Improved tool compatibility
    Improved python tool for compatibility with SSD1306 ESP Driver bitmap drawing

--- tools/python-bmp-to-c-array/pngtocarray.py
+++ tools/python-bmp-to-c-array/pngtocarray.py
@@ -1,7 +1,7 @@
 import os, sys
 from PIL import Image
 
-path = "C://Users//parek//OneDrive//Desktop//lasertag assets"
+path = "C://Users//parek//OneDrive//Desktop//LTA"
 
 f = open(path + "//header.h", "w")
 
@@ -9,14 +9,24 @@ for File in os.listdir(path):
     try:
         im = Image.open(path + "//" + File, 'r')
         pix = im.load()
+        arrayDim = int((im.height * im.width) / 8)
         f.write("//" + File + " " + str(im.height) + "x" + str(im.width) + "px\n")
-        f.write("const bool " + File[0:-4] + " [" + str(im.height) + "][" + str(im.width) + "] {\n")
+        f.write("const uint8_t " + File[0:-4] + "[" + str(arrayDim) + "] = {\n")
+        idx = 0
+        byte = ""
         for hidx in range(im.height):
             for widx in range(im.width):
-                if hidx == 0 and widx == 0:
-                    f.write(str(pix[widx, hidx]))
-                else:
-                    f.write(", " + str(pix[widx, hidx]))
+                byte += str(pix[widx, hidx])
+                idx += 1
+                if idx % 8 == 0:
+                    byteValue = hex(int(byte, 2))
+
+                    if idx == 8:
+                        f.write(str(byteValue))
+                    else:
+                        f.write(", " + str(byteValue))
+
+                    byte = ""
 
         f.write("\n};\n\n")
     except OSError:
